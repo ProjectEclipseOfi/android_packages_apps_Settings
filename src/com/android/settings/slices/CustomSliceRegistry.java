fa16961a8a17297ded2d4b83b2646631c19e631d
Techoraye <robergem2008@gmail.com>
2025-12-01 06:39:16 -0500

    Fixed ready to use!

--- src/com/android/settings/slices/CustomSliceRegistry.java
+++ src/com/android/settings/slices/CustomSliceRegistry.java
@@ -33,7 +33,6 @@ import com.android.settings.homepage.contextualcards.slices.DarkThemeSlice;
 import com.android.settings.homepage.contextualcards.slices.FaceSetupSlice;
 import com.android.settings.homepage.contextualcards.slices.LowStorageSlice;
 import com.android.settings.location.LocationSlice;
-import com.android.settings.media.AppVolumeSlice;
 import com.android.settings.media.MediaOutputIndicatorSlice;
 import com.android.settings.media.RemoteMediaSlice;
 import com.android.settings.network.ProviderModelSlice;
@@ -316,7 +315,6 @@ public class CustomSliceRegistry {
         sUriToSlice.put(ALWAYS_ON_SLICE_URI, AlwaysOnDisplaySlice.class);
         sUriToSlice.put(MEDIA_OUTPUT_INDICATOR_SLICE_URI, MediaOutputIndicatorSlice.class);
         sUriToSlice.put(REMOTE_MEDIA_SLICE_URI, RemoteMediaSlice.class);
-        sUriToSlice.put(APP_VOLUME_SLICE_URI, AppVolumeSlice.class);
 
         // Slices for contextual card.
         sUriToSlice.put(FACE_ENROLL_SLICE_URI, FaceSetupSlice.class);
